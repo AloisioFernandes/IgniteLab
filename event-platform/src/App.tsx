d88d349a63b7681eb4a068823c530a9626d508cf
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-08-15 22:09:45 -0300

    Requisições com apollo.

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
 function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
+
   return (
     <h1>Hello World</h1>
   )
