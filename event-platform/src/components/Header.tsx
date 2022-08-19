b44c43f923f2b317519ca6421bddfd7f6b9d97b8
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-08-19 20:32:49 -0300

    Cores tailwind.

--- event-platform/src/components/Header.tsx
+++ event-platform/src/components/Header.tsx
@@ -2,6 +2,8 @@ import { Logo } from "./Logo";
 
 export function Header() {
   return (
-    <Logo />
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700">
+      <Logo />
+    </header>
   )
 }
