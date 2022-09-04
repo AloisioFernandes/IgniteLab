3b35cabd9a8dd11947e6a63406cb880f19b2cce2
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-09-03 21:33:50 -0300

    Componente professor.

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -13,9 +13,22 @@ export function Video() {
             <h1 className="text-2xl font-bold">
               Aula 01 - Abertura do Ignite Lab
             </h1>
-            <p className="mt-4 text-gray-200">
+            <p className="mt-4 text-gray-200 leading-relaxed">
               Descrição da primeira aula do ignite lab.
             </p>
+
+            <div className="flex item-center gap-4 mt-6">
+              <img 
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="https://github.com/diego3g.png" 
+                alt="" 
+              />
+
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">Diego Fernandes</strong>
+                <span className="text-gray-200 text-sm block">CTO @ Rocketseat</span>
+              </div>
+            </div>
           </div>
 
           <div className="flex flex-col gap-4">
@@ -23,6 +36,7 @@ export function Video() {
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
+
             <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
               <Lightning size={24} />
               Acesse o desafio
@@ -30,7 +44,6 @@ export function Video() {
           </div>
         </div>
         <div>
-
         </div>
       </div>
     </div>
