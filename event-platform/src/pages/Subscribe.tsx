fe2132d4897b080e83cf0c54444af077f883c611
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-09-14 20:59:13 -0300

    Componente form.

--- event-platform/src/pages/Subscribe.tsx
+++ event-platform/src/pages/Subscribe.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../components/Logo";
 export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
           <Logo />
 
@@ -13,6 +13,21 @@ export function Subscribe() {
           <p className="mt-4 text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
+        </div>
+
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+
+          <form action="" className="flex flex-col gap-2 w-full">
+            <input 
+              type="text" 
+              placeholder="Seu nome completo" 
+            />
+            <input 
+              type="text" 
+              placeholder="Digite seu e-mail" 
+            />
+          </form>
         </div>
       </div>
 
