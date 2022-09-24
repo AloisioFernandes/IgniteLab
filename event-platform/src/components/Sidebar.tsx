e62e687f8ccbb3d9acb5ba79db3d1a2f211f8c4d
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-09-23 21:00:46 -0300

    Codegen.

--- event-platform/src/components/Sidebar.tsx
+++ event-platform/src/components/Sidebar.tsx
@@ -1,8 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+  const { data } = useGetLessonsQuery()
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
@@ -16,7 +16,7 @@ export function Sidebar() {
             <Lesson
               key={lesson.id}
               title={lesson.title}
-              slug={lesson.slug}
+              slug={lesson.slug || ""}
               availableAt={new Date(lesson.availableAt)}
               type={lesson.lessonType}
             />
